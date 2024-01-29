47e38c28e9efcd01fba905ec42694471b5cb89d8
RoyalRoader <sksksdlqk@naver.com>
2024-01-29 16:10:13 +0900

    pom.xml 수정(test.java)이름 통일

--- secondProducer/src/test/java/producer/MainrunningappTests.java
+++ secondProducer/src/test/java/producer/MainrunningappTests.java
@@ -1,10 +1,10 @@
-package com.example.secondProducer;
+package producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecondProducerApplicationTests {
+class MainrunningappTests {
 
 	@Test
 	void contextLoads() {
